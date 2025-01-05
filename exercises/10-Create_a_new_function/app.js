2222a964700d3c3b92f074b90ea12895ee0baf44
Francisco Yuster <yusterfrancisco@gmail.com>
2025-01-05 03:18:05 +0000

    finish him

--- exercises/10-Create_a_new_function/app.js
+++ exercises/10-Create_a_new_function/app.js
@@ -1,7 +1,7 @@
-function shortIntroduction() {
+function shortIntroduction(Name,Profession,Age) {
   // Complete this function's body and arguments
-  
+  return "Hello! my name is "+ Name + ", my profession is " + Profession +". I am "+ Age+" years old." 
 }
 
 // Fill the gaps with your data in the correct order
-console.log(shortIntroduction(" ", " ", " "))
+console.log(shortIntroduction("Francisco", "MBA", "21"))
